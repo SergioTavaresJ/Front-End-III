7b6ad7763c0eb3b4451eb759f0fe38b83d8c21c3
SergioTavares <112196923+SergioTavaresJ@users.noreply.github.com>
2024-03-06 16:43:44 -0300

    Add files via upload

---
+++ revisaojs/Atividade1.js
@@ -0,0 +1,12 @@
+var nome = "Sérgio";
+const cpf = "951282785-34";
+
+function InformacoesDoCliente(){
+    let sobrenome = "Tavares";
+    return " Nome:"+ nome +
+    "\n Sobrenome:" + sobrenome +
+    "\n CPF:"+ cpf;
+}
+
+
+console.log(InformacoesDoCliente());
